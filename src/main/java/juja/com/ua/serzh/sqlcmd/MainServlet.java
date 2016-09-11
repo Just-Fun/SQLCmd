bb150d4ee739e75ebe4e4a87c88d4710ac08a43d
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-11 17:01:11 +0300

    Start project!

---
+++ src/main/java/juja/com/ua/serzh/sqlcmd/MainServlet.java
@@ -0,0 +1,31 @@
+package juja.com.ua.serzh.sqlcmd;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Serzh on 9/11/16.
+ */
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        super.doPost(req, resp);
+    }
+
+}

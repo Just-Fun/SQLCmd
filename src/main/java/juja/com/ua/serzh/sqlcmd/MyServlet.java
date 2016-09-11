ec8d30491db0fe299e6a5a287e968d1b1a48272d
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-11 18:11:23 +0300

    подключили 2 сервлета :)

---
+++ src/main/java/juja/com/ua/serzh/sqlcmd/MyServlet.java
@@ -0,0 +1,32 @@
+package juja.com.ua.serzh.sqlcmd;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Serzh on 9/11/16.
+ */
+@WebServlet("/s")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("Hello Word");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
+

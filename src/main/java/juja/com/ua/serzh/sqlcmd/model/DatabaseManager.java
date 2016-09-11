cda7283fdb604c1f9a4af14d558f30a9441533cf
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-11 18:56:32 +0300

    add dao

---
+++ src/main/java/juja/com/ua/serzh/sqlcmd/model/DatabaseManager.java
@@ -0,0 +1,45 @@
+package juja.com.ua.serzh.sqlcmd.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by serzh on 5/11/16.
+ */
+public interface DatabaseManager {
+
+    List<Map<String, Object>> getTableData(String tableName);
+
+    Set<String> getDatabases();
+
+    Set<String> getTableNames();
+
+    void connect(String database, String userName, String password);
+
+    void clear(String tableName);
+
+    void createDatabase(String databaseName);
+
+    void dropDatabase(String databaseName);
+
+    void createTable(String query);
+
+    void dropTable(String query);
+
+    void insert(String tableName, Map<String, Object> input);
+
+    void update(String tableName, int id, Map<String, Object> newValue);
+
+    Set<String> getTableColumns(String tableName);
+
+    boolean isConnected();
+
+    String getUser();
+
+    String getPassword();
+
+    String getDatabaseName();
+
+    int getTableSize(String tableName);
+}
---
+++ src/main/java/juja/com/ua/serzh/sqlcmd/model/DatabaseManagerException.java
@@ -0,0 +1,12 @@
+package juja.com.ua.serzh.sqlcmd.model;
+
+/**
+ * Created by serzh on 31.05.16.
+ */
+public class DatabaseManagerException extends RuntimeException {
+
+    public DatabaseManagerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+

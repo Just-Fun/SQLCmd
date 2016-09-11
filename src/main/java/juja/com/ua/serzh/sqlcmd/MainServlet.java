e3ffeda4d1fc23be8a6177660c58192d6b8a48d9
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-11 17:16:21 +0300

    использовали аннотацию, отключили файл .xml

--- src/main/java/juja/com/ua/serzh/sqlcmd/MainServlet.java
+++ src/main/java/juja/com/ua/serzh/sqlcmd/MainServlet.java
@@ -1,6 +1,7 @@
 package juja.com.ua.serzh.sqlcmd;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,13 +11,14 @@ import java.io.PrintWriter;
 /**
  * Created by Serzh on 9/11/16.
  */
+@WebServlet("/")
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        req.setAttribute("name", "Devcolibri");
+        req.setAttribute("name", "Devcolibri3");
 
         req.getRequestDispatcher("mypage.jsp").forward(req, resp);
 

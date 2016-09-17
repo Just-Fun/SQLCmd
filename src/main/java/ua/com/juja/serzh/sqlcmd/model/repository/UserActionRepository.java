72278cd029e5eeeb23db7ddbf79afd11ac41bfbf
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-17 15:05:04 +0300

    UserAction "ORDER BY date DESC"

--- src/main/java/ua/com/juja/serzh/sqlcmd/model/repository/UserActionRepository.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/model/repository/UserActionRepository.java
@@ -1,8 +1,10 @@
 package ua.com.juja.serzh.sqlcmd.model.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ua.com.juja.serzh.sqlcmd.model.entity.UserAction;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public interface UserActionRepository extends CrudRepository<UserAction, Integer>, UserActionRepositoryCustom {
@@ -10,6 +12,6 @@ public interface UserActionRepository extends CrudRepository<UserAction, Integer
   /*  @Query(value = "SELECT ua FROM UserAction ua WHERE ua.connection.userName = :userName")
     List<UserAction> findByUserName(@Param("userName") String userName);*/
 
-//    @Query(value = "SELECT ua FROM UserAction ua")
+    @Query(value = "SELECT ua FROM UserAction ua ORDER BY date DESC")
     List<UserAction> findAll();
 }

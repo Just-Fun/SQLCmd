9b4e829d2cf5259dc11cf1511358904cdda1f457
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-17 17:50:24 +0300

    limit printing userActions

--- src/main/java/ua/com/juja/serzh/sqlcmd/model/repository/UserActionRepository.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/model/repository/UserActionRepository.java
@@ -1,14 +1,13 @@
 package ua.com.juja.serzh.sqlcmd.model.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ua.com.juja.serzh.sqlcmd.model.entity.UserAction;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public interface UserActionRepository extends PagingAndSortingRepository<UserAction, Integer>, UserActionRepositoryCustom {
 
     @Query(value = "SELECT ua FROM UserAction ua ORDER BY date DESC")
-    List<UserAction> findAll();
+    Page<UserAction> findAll(Pageable pageable);
 }

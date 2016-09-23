a85617ba5ef16539e9b9f1222eecd79735b1ae14
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-23 13:11:37 +0300

    create DatabaseManagerTest

--- src/main/java/ua/com/juja/serzh/sqlcmd/Temp.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/Temp.java
@@ -1,39 +1,20 @@
 package ua.com.juja.serzh.sqlcmd;
 
+import ua.com.juja.serzh.sqlcmd.dao.databaseManager.PostgreSQLManager;
+
+import java.util.Set;
+
 /**
  * Created by Serzh on 9/12/16.
  */
 public class Temp {
 
-    private int i = 0;
-    private String s = "";
-
     public static void main(String[] args) {
-        System.out.println("Hi!");
-    }
-
-    private static void some() {
-    }
-
-    private void some(int i) {
-    }
-
-    private static void some(String i) {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) return true;
-        if (o == null || o.getClass() != getClass()) return false;
-        Temp temp = (Temp) o;
-        if (i != temp.i) return false;
-        return s.equals(temp.s);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = i;
-        result = 31 * result + s.hashCode();
-        return result;
+        PostgreSQLManager manager = new PostgreSQLManager();
+        manager.connect("", "postgres", "postgres");
+        manager.dropDatabase("new1");
+        manager.dropDatabase("new2");
+        Set<String> databases = manager.getDatabases();
+        databases.forEach(System.out::println);
     }
 }
---
+++ src/test/java/ua/com/juja/serzh/sqlcmd/dao/DatabaseManagerTest.java
@@ -0,0 +1,165 @@
+package ua.com.juja.serzh.sqlcmd.dao;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import ua.com.juja.serzh.sqlcmd.Setup;
+import ua.com.juja.serzh.sqlcmd.dao.databaseManager.DatabaseManager;
+import ua.com.juja.serzh.sqlcmd.dao.databaseManager.PostgreSQLManager;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by serzh on 5/11/16.
+ */
+@Ignore
+public class DatabaseManagerTest {
+
+    private static Setup setup;
+    private static DatabaseManager manager;
+    private static String database;
+    private static String user;
+    private static String password;
+
+    @BeforeClass
+    public static void setup() {
+        setup = new Setup();
+        database = "sqlcmd5hope5never5exist";
+        user = setup.getUser();
+        password = setup.getPassword();
+
+        manager = new PostgreSQLManager();
+        setup.setupData(manager, database);
+    }
+
+    @AfterClass
+    public static void dropDatabase() {
+        setup.dropData(manager);
+    }
+
+    @Test
+    public void testGetAllTableNames() {
+        Set<String> tables = manager.getTableNames();
+        assertEquals("[users, users2, test1]", tables.toString());
+    }
+
+    @Test
+    public void testGetTableData() {
+        // given
+        manager.clear("users");
+        // when
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("name", "Stiven");
+        input.put("password", "****");
+        input.put("id", 11);
+        manager.insert("users", input);
+        // then
+        List<Map<String, Object>> users = manager.getTableData("users");
+        assertEquals(1, users.size());
+
+        Map<String, Object> user = users.get(0);
+        assertEquals("[Stiven, ****, 11]", user.values().toString());
+        assertEquals("[name, password, id]", user.keySet().toString());
+    }
+
+    @Test
+    public void testGetTableData2() {
+        // given
+        manager.clear("users");
+        // when
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("name", "Stiven");
+        input.put("password", "****");
+        input.put("id", 11);
+        manager.insert("users", input);
+
+        Map<String, Object> input2 = new LinkedHashMap<>();
+        input2.put("name", "Stiven2");
+        input2.put("password", "*****");
+        input2.put("id", 12);
+        manager.insert("users", input2);
+        // then
+        List<Map<String, Object>> users = manager.getTableData("users");
+        assertEquals(2, users.size());
+
+        Map<String, Object> user = users.get(0);
+        assertEquals("[Stiven, ****, 11]", user.values().toString());
+        assertEquals("[name, password, id]", user.keySet().toString());
+
+        Map<String, Object> user2 = users.get(1);
+        assertEquals("[Stiven2, *****, 12]", user2.values().toString());
+        assertEquals("[name, password, id]", user2.keySet().toString());
+    }
+
+    @Test
+    public void testUpdateTableData() {
+        // given
+        manager.clear("users");
+
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("name", "Stiven");
+        input.put("password", "pass");
+        input.put("id", 15);
+        manager.insert("users", input);
+        // when
+        Map<String, Object> newValue = new LinkedHashMap<>();
+        newValue.put("password", "pass2");
+        newValue.put("name", "Pup");
+        manager.update("users", "id",  "15", newValue);
+        // then
+        List<Map<String, Object>> users = manager.getTableData("users");
+        assertEquals(1, users.size());
+
+        Map<String, Object> user = users.get(0);
+        assertEquals("[name, password, id]", user.keySet().toString());
+        assertEquals("[Pup, pass2, 15]", user.values().toString());
+    }
+
+    @Test
+    public void testGetColumnNames() {
+        // when
+        Set<String> columnNames = manager.getTableColumns("users");
+        // then
+        assertEquals("[name, password, id]", columnNames.toString());
+    }
+
+    @Test
+    public void getTableSize() {
+//        int size = manager.getTableSize("users");
+//        assertEquals(1, size);
+    }
+
+    @Test
+    public void clearTable() {
+        // given
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("name", "Stiven");
+        input.put("password", "pass");
+        input.put("id", 17);
+        manager.insert("users", input);
+        // when
+        manager.clear("users");
+        // then
+        List<Map<String, Object>> users = manager.getTableData("users");
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    public void testIsConnected() {
+        assertTrue(manager.isConnected());
+    }
+
+    @Test
+    public void dropTable() {
+        manager.dropTable("test1");
+        Set<String> tables = manager.getTableNames();
+        assertEquals("[users, users2]", tables.toString());
+        manager.createTable("test1(id SERIAL NOT NULL PRIMARY KEY,username varchar(225) NOT NULL UNIQUE, password varchar(225))");
+    }
+}
